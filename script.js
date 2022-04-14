fcf5f40e67d86496152da61a23b8e94de7b5ffe9
AdamSakeA <101402228+AdamSakeA@users.noreply.github.com>
2022-04-14 09:50:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,14 @@ const btnInput = document.querySelector('.input');
 const inputTask = document.querySelector('.inputTask');
 const newTask = document.querySelector('#tasks');
 const todoList = document.querySelector('.todoList');
+const alert = document.querySelector('.alertTask');
 
 btnInput.addEventListener('click', () => {
     if(inputTask.value.length === 0){
-        alert('Please input your task!')
+        alert.innerHTML = `Please Input your Task!`;
+        alert.style.color = 'red';
     } else {
+        alert.innerHTML = '';
         newTask.innerHTML += `
             <div class="task">
                 <span class="taskName">
